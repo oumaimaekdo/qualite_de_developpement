2c93b0203c60e5258f1b6febce8e13a3978c0403
EL KHADRAOUI Oumaima <12405655@q10417.iutv.univ-paris13.fr>
2025-05-23 10:50:50 +0200

    ajout test division + multiplication

--- src/test/TestCalculatrice.java
+++ src/test/TestCalculatrice.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestCalculatrice {
+
     private Calculatrice calc;
 
     @BeforeEach
@@ -13,5 +14,15 @@ public class TestCalculatrice {
     @Test
     public void whenAddingFourAndSix_ShouldReturnTen() {
         assertEquals(10, calc.add(4, 6));
+    }
+
+    @Test
+    public void whenDividingFortyByTwo_ShouldReturnTwenty() {
+        assertEquals(20, this.calc.div(40, 2));
+    }
+
+    @Test
+    public void whenMultiplyingMinusThreeAndFour_ShouldReturnMinusTwelve() {
+        assertEquals(-12, this.calc.mult(-3, 4));
     }
 }
